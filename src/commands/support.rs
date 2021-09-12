fa8d00e7e020aaa87afacd0d6aac23a9be98d8e4
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-09-12 09:55:18 -0400

    Automatically add helpers to support thread (may change in the future as it pings sadly)

--- src/commands/support.rs
+++ src/commands/support.rs
@@ -1,6 +1,6 @@
 use super::Context;
 use anyhow::Result;
-use serenity::futures::StreamExt;
+use serenity::futures::{future, StreamExt};
 use serenity::model::{
     channel::{ChannelType, Message},
     id::{ChannelId, GuildId, RoleId},
@@ -11,12 +11,10 @@ pub async fn create_new(ctx: Context<'_>, message: Message) -> Result<()> {
     let uuid: String = Uuid::new_v4().to_string()[..6].to_string();
     let support_channel = ChannelId(ctx.data().config.env.support_channel_id);
 
-    println!("{}", uuid);
-
     let thread = support_channel
         .create_public_thread(&ctx.discord().http, message.id, |t| {
             t.name("case-".to_string() + &uuid);
-            t.auto_archive_duration(60);
+            t.auto_archive_duration(1440);
             t.kind(ChannelType::PublicThread);
 
             t
@@ -31,6 +29,27 @@ pub async fn create_new(ctx: Context<'_>, message: Message) -> Result<()> {
             m
         })
         .await?;
+
+    let helpers = GuildId(*message.guild_id.unwrap().as_u64())
+        .members_iter(&ctx.discord().http)
+        .filter(|u| {
+            if let Some(u) = u.as_ref().ok() {
+                future::ready(
+                    u.roles
+                        .contains(&RoleId(ctx.data().config.env.helper_role_id)),
+                )
+            } else {
+                future::ready(false)
+            }
+        });
+
+    for h in helpers.collect::<Vec<_>>().await.iter() {
+        let helper = h.as_ref().unwrap();
+
+        thread
+            .add_thread_member(&ctx.discord().http, helper.user.id)
+            .await?;
+    }
 
     Ok(())
 }
