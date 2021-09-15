88addb7dc5caee4fd9d8f22f7958eed4079947e9
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-09-14 22:54:07 -0400

    Register call command

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -12,7 +12,7 @@ pub fn command_list(
     options.command(meta::help(), |f| f.category("Meta"));
     options.command(meta::source(), |f| f.category("Meta"));
 
-    // options.command(support::close(), |f| f.category("Support"));
+    options.command(support::call(), |f| f.category("Support"));
 
     Ok(options)
 }
